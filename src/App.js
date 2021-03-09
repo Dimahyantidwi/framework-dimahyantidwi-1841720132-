a0b3b180fa4f4274f1625bdd4af2c35d771dcbae
Dimahyanti dwi <Dimahyantidwi@gmail.com>
2021-03-09 14:36:42 +0800

    Modul 5

--- src/App.js
+++ src/App.js
@@ -1,133 +1,72 @@
-import React from "react";
+import React from "raect";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link,
-  Redirect,
-  useHistory,
-  useLocation
+  Link
 } from "react-router-dom";
 
-// Pada aplikasi ini memiliki 3 halaman: public page, private page, dan halaman login.
-//Untuk masuk ke private page, Anda harus login terlebih dahulu.
+// Situs ini memiliki 3 halaman, yang semuanya dirender secara dinamis di browser.
+// Meskipun halaman tidak pernah di refresh, perhatikan bagaimana React Router
+// membuat URL selalu terbarui saat anda menavigasi situs.
+// Ini menjadi riwayat browser, memastikan hal-hal seperti tombol kembali
+// dan bookmark berfungsi dengan baik.
 
-// Pertama, klik public page. Kemudian, kunjungi private page.
-//karena anda belum login, jadi anda diarahkan ke halaman login.
-//Setelah login, anda akan diarahlan kembali ke private page.
-
-// Perhatikan perubahan setiap URL, Jika anda mengklik tombol kembali,
-//apakah anda kembali ke halaman login? Tidak, karena anda sudah login.
-//Cobalah, maka anda akan kembali ke halaman yang anda kunjungi sebelum login, yaitu public page.
-
-export default function AuthExample() {
+export default function BasicExample() {
   return (
     <Router>
       <div>
-        <AuthButton />
-
         <ul>
           <li>
-            <Link to="/public">Public Page</Link>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/private">Private Page</Link>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
           </li>
         </ul>
+        <hr />
 
         <Switch>
-          <Route path="/public">
-            <PublicPage />
+          <Route exact path="/">
+            <Home />
           </Route>
-          <Route path="/login">
-            <LoginPage />
+          <Route path="/about">
+            <About />
           </Route>
-          <PrivateRoute path="/private">
-            <ProtectedPage />
-          </PrivateRoute>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
         </Switch>
       </div>
     </Router>
   );
 }
 
-const fakeAuth = {
-  isAuthenticated: false,
-  authenticate(cb) {
-    fakeAuth.isAuthenticated = true;
-    setTimeout(cb, 100); // fake async
-  },
-  signout(cb) {
-    fakeAuth.isAuthenticated = false;
-    setTimeout(cb, 100);
-  }
-};
+// Anda dapat menganggap komponen ini sebagai "halaman(konten)" di situs Anda.
 
-function AuthButton() {
-  let history =  useHistory();
-
-  return fakeAuth.isAuthenticated ? (
-    <p>
-      Welcome!{" "}
-      <button
-        onClick={() => {
-          fakeAuth.signout(() => history.push("/"));
-        }}
-      >
-        Sign out
-      </button>
-    </p>
-  ) : (
-    <p>You are not logged in.</p>
-  );
-}
-
-// Pembungkus untuk <Route> yang mengarahkan ke login
-// tampilan jika Anda belum terkonfirmasi.
-
-function PrivateRoute({ children, ...rest}) {
-  return (
-    <Route
-      {...rest}
-      render={({ location }) =>
-        fakeAuth.isAuthenticated ? (
-          children
-        ) : (
-          <Redirect
-            to={{
-              pathname: "/login",
-              state: { from: location }
-            }}
-          />
-        )
-      }
-    />
-  );
-}
-
-function PublicPage() {
-  return <h3>Public</h3>;
-}
-
-function ProtectedPage() {
-  return <h3>Private</h3>;
-}
-
-function LoginPage() {
-  let history = useHistory();
-  let location = useLocation();
-
-  let { from } = location.state || { from: { pathname: "/" } };
-  let login = () => {
-    fakeAuth.authenticate(() => {
-      history.replace(from);
-    });
-  };
-
+function Home() {
   return (
     <div>
-      <p>You must log in to view the page at {from.pathname}</p>
-      <button onClick={login}>Log in</button>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
     </div>
   );
 }
