2ea0c7057638d4cec9ae0241d73aa842ab2c9e89
user <dimahyantidwi@gmail.com>
2021-04-23 21:28:31 +0800

    Modul 10

---
+++ modul/src/firebase.config.js
@@ -0,0 +1,9 @@
+ export default {
+    apiKey: "AIzaSyAdSGQr7Xrh6USF0ckfjXbMAL0MhcjKXeM",
+    authDomain: "modul10-74562.firebaseapp.com",
+    projectId: "modul10-74562",
+    storageBucket: "modul10-74562.appspot.com",
+    messagingSenderId: "991261544234",
+    appId: "1:991261544234:web:f1d37c19a2b83310c39853",
+    measurementId: "G-REDE4F8L3M"
+};
